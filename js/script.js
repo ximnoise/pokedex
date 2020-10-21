bdfe4af4c1a63ee398d94b87d61aa553bd88ea2c
ximnoise <36673383+ximnoise@users.noreply.github.com>
2020-10-21 16:23:01 +0200

    added for loop to display array + basic styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
 pokemonList = [
-  {name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']},
-  {name: "Ivysaur", height: 1, type: ['grass', 'poison']},
-  {name: "Venusaur", height: 2, type: ['grass', 'poison']},
-  {name: "Charmander", height: 0.6, type: ['fire']},
-  {name: "Charmeleon", height: 1.1, type: ['fire']},
-  {name: "Charizard", height: 1.7, type: ['fire', 'flying']}
+  {
+    name: "Bulbasaur", 
+    height: 0.7, 
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Ivysaur", 
+    height: 1, 
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Venusaur", 
+    height: 2, 
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Charmander", 
+    height: 0.6, 
+    type: ['fire']
+  },
+  {
+    name: "Charmeleon", 
+    height: 1.1, 
+    type: ['fire']
+  },
+  {
+    name: "Charizard", 
+    height: 1.7, 
+    type: ['fire', 'flying']
+  }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 2) {
+    document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + "m) " + "- Wow, that's big" + "<br>");
+  } else {
+    document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + 'm)' + '<br>');
+  }
+}
