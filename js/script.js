7d4d91e823fc08fd474a798e608b4ea77fa926a0
ximnoise <36673383+ximnoise@users.noreply.github.com>
2020-10-27 09:30:27 +0100

    added IIFE

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
-pokemonList = [
-  {
-    name: "Bulbasaur", 
-    height: 0.7, 
-    type: ['grass', 'poison']
-  },
-  {
-    name: "Ivysaur", 
-    height: 1, 
-    type: ['grass', 'poison']
-  },
-  {
-    name: "Venusaur", 
-    height: 2, 
-    type: ['grass', 'poison']
-  },
-  {
-    name: "Charmander", 
-    height: 0.6, 
-    type: ['fire']
-  },
-  {
-    name: "Charmeleon", 
-    height: 1.1, 
-    type: ['fire']
-  },
-  {
-    name: "Charizard", 
-    height: 1.7, 
-    type: ['fire', 'flying']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ['grass', 'poison'] },
+    { name: "Ivysaur", height: 1, type: ['grass', 'poison'] },
+    { name: "Venusaur", height: 2, type: ['grass', 'poison'] },
+    { name: "Charmander", height: 0.6, type: ['fire'] },
+    { name: "Charmeleon", height: 1.1, type: ['fire']},
+    { name: "Charizard", height: 1.7, type: ['fire', 'flying'] }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
 
 function loopPokemonList(pokemon) {
   if (pokemon.height >= 2) {
@@ -38,4 +31,4 @@ function loopPokemonList(pokemon) {
     document.write("<p>" + pokemon.name + "(height: " + pokemon.height + "m)" + "</p>");
   }
 }
-pokemonList.forEach(loopPokemonList);
+pokemonRepository.getAll().forEach(loopPokemonList);
